712856907c01c0edf6c71eaa42523eccbaf73df3
Eleven <idiotalex@163.com>
2021-03-12 17:33:58 +0800

    add data-cache modules

---
+++ linux-monitor-data-cache/linux-monitor-data-cache-impl/src/main/java/com/hotstrip/linux/monitor/data/cache/impl/BaseDataCache.java
@@ -0,0 +1,26 @@
+package com.hotstrip.linux.monitor.data.cache.impl;
+
+import com.google.common.collect.Maps;
+
+import java.util.Optional;
+import java.util.concurrent.ConcurrentMap;
+
+public class BaseDataCache {
+
+    private BaseDataCache() {
+    }
+
+    private static class BaseDataCacheHolder {
+        private static final BaseDataCache INSTANCE = new BaseDataCache();
+    }
+
+    public static final BaseDataCache getInstance() {
+        return BaseDataCacheHolder.INSTANCE;
+    }
+
+    private static final ConcurrentMap<String, Object> PLUGIN_MAP = Maps.newConcurrentMap();
+
+    public void cachePluginData(final Object pluginData) {
+        Optional.ofNullable(pluginData).ifPresent(data -> PLUGIN_MAP.put(data.toString(), data));
+    }
+}
