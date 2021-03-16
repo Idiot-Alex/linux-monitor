6d8c57b0d5dc2abef97633775b3d15982d0ba55f
Eleven <idiotalex@163.com>
2021-03-16 21:00:24 +0800

    add pagehelper

---
+++ linux-monitor-admin/src/main/java/com/hotstrip/linux/monitor/admin/service/impl/AdminUserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.hotstrip.linux.monitor.admin.service.impl;
+
+import com.hotstrip.linux.monitor.admin.service.AdminUserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminUserServiceImpl implements AdminUserService {
+}
