93234941031300354bb77ba1db3bf03ddde257e7
Eleven <idiotalex@163.com>
2021-03-18 15:24:06 +0800

    add element ui

--- linux-monitor-dashboard-vue/src/main.js
+++ linux-monitor-dashboard-vue/src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+import {Button} from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.config.productionTip = false;
+Vue.use(Button);
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
