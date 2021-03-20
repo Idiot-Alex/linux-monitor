4f5aa8d9f651291ada05bc2665555582bb7bb17c
Eleven <idiotalex@163.com>
2021-03-20 15:57:26 +0800

    update pom.xml

--- linux-monitor-plugins/linux-monitor-plugin-ssh/src/main/java/com/hotstrip/linux/monitor/plugin/ssh/SSHClientService.java
+++ linux-monitor-plugins/linux-monitor-plugin-ssh/src/main/java/com/hotstrip/linux/monitor/plugin/ssh/SSHClientService.java
@@ -1,6 +1,7 @@
 package com.hotstrip.linux.monitor.plugin.ssh;
 
 import com.hotstrip.linux.monitor.common.LinuxMonitorThreadFactory;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Objects;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
@@ -10,6 +11,7 @@ import java.util.concurrent.TimeUnit;
  * @author Hotstrip
  * SSH client service
  */
+@Slf4j
 public class SSHClientService implements AutoCloseable {
 
     private final ScheduledThreadPoolExecutor executor;
@@ -18,7 +20,7 @@ public class SSHClientService implements AutoCloseable {
         this.executor = new ScheduledThreadPoolExecutor(10, LinuxMonitorThreadFactory.create("ssh-client", true));
 
         this.executor.scheduleWithFixedDelay(() -> {
-
+            log.info("schedule with fixed delay task...");
         }, 10, 30, TimeUnit.SECONDS);
     }
 
--- linux-monitor-spring-boot-starter/linux-monitor-spring-boot-starter-ssh/src/main/java/com/hotstrip/linux/monitor/spring/boot/ssh/SSHConfiguration.java
+++ linux-monitor-spring-boot-starter/linux-monitor-spring-boot-starter-ssh/src/main/java/com/hotstrip/linux/monitor/spring/boot/ssh/SSHConfiguration.java
@@ -1,7 +1,8 @@
 package com.hotstrip.linux.monitor.spring.boot.ssh;
 
-import com.hotstrip.linux.monitor.plugin.ssh.session.SSHSessionService;
+import com.hotstrip.linux.monitor.plugin.ssh.SSHClientService;
 import com.hotstrip.linux.monitor.plugin.ssh.session.DefaultSSHSessionServiceImpl;
+import com.hotstrip.linux.monitor.plugin.ssh.session.SSHSessionService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,5 +12,10 @@ public class SSHConfiguration {
     @Bean
     public SSHSessionService sshSessionService() {
         return new DefaultSSHSessionServiceImpl();
+    }
+
+    @Bean
+    public SSHClientService sshClientService() {
+        return new SSHClientService();
     }
 }
