f3fd7cacbb71188cad1e446ee7d89aabec437e0f
Eleven <idiotalex@163.com>
2021-03-21 21:07:32 +0800

    remove data cache modules

--- linux-monitor-data-cache/linux-monitor-dada-cache-api/src/main/java/com/hotstrip/linux/monitor/data/cache/api/BaseDataCacheService.java
+++
@@ -1,8 +0,0 @@
-package com.hotstrip.linux.monitor.data.cache.api;
-
-import com.hotstrip.linux.monitor.common.pojo.SystemLoadAvgDO;
-
-public interface BaseDataCacheService {
-
-    void cacheSystemLoadAvgData(final SystemLoadAvgDO systemLoadAvgDO);
-}
