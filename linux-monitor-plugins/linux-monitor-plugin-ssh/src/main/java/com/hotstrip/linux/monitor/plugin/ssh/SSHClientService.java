f6dcb2b2808a60ba9ec40aec4036d432c94dfd4e
Eleven <idiotalex@163.com>
2021-03-21 21:28:18 +0800

    update SSHClientService code

--- linux-monitor-plugins/linux-monitor-plugin-ssh/src/main/java/com/hotstrip/linux/monitor/plugin/ssh/SSHClientService.java
+++ linux-monitor-plugins/linux-monitor-plugin-ssh/src/main/java/com/hotstrip/linux/monitor/plugin/ssh/SSHClientService.java
@@ -1,9 +1,14 @@
 package com.hotstrip.linux.monitor.plugin.ssh;
 
 import com.hotstrip.linux.monitor.common.LinuxMonitorThreadFactory;
+import com.hotstrip.linux.monitor.plugin.ssh.executor.ChannelExecutor;
 import com.hotstrip.linux.monitor.plugin.ssh.executor.Executor;
 import com.hotstrip.linux.monitor.plugin.ssh.executor.handler.LoadAvgHandler;
+import com.hotstrip.linux.monitor.plugin.ssh.session.ConstPool;
 import com.hotstrip.linux.monitor.plugin.ssh.session.SSHSessionManage;
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 import lombok.extern.slf4j.Slf4j;
 
@@ -28,7 +33,16 @@ public class SSHClientService implements AutoCloseable {
             for (Session session : sessionList) {
                 if (session.isConnected()) {
                     log.info("schedule with fixed delay task...host: [{}]", session.getHost());
+                    try {
+                        Channel channel = session.openChannel(ConstPool.EXEC_CHANNEL);
+                        // 获取 Mac OS 系统的平均负载命令 uptime | cut -d":" -f4- | sed s/,//g
+                        ((ChannelExec) channel).setCommand("uptime | cut -d\":\" -f4- | sed s/,//g");
 
+                        Executor executor = new ChannelExecutor((ChannelExec) channel);
+                        executor.execute(new LoadAvgHandler());
+                    } catch (JSchException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }, 10, 30, TimeUnit.SECONDS);
