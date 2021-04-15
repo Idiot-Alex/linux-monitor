4151d8133ad33003f843143ea811519d23bac690
Eleven <idiotalex@163.com>
2021-04-16 00:12:24 +0800

    add antv g2 chart

--- linux-monitor-dashboard-vue/src/pages/server/list.vue
+++ linux-monitor-dashboard-vue/src/pages/server/list.vue
@@ -22,12 +22,14 @@
           <el-progress class="five" type="circle" :show-text="false" :percentage="calcLoadAvg(server.five, server.cores)" :width="60"></el-progress>
           <el-progress class="fifteen" type="circle" :show-text="false" :percentage="calcLoadAvg(server.fiften, server.cores)" :width="40"></el-progress>
         </div>
+        <div id="container"></div>
       </el-card>
     </el-row>
   </div>
 </template>
 <script>
 import { getServerList, getServerProperties } from '../../api/server';
+import { Chart } from '@antv/g2';
 export default {
   data() {
     return {
@@ -43,6 +45,9 @@ export default {
   created() {
     this.loadData();
     this.loadProperties();
+    setTimeout(() => {
+      this.drawLoadAvgChart();
+    }, 2000)
   },
   beforeDestroy() {
     if (this.timer) {
@@ -82,6 +87,7 @@ export default {
         }, 3000);
       }
     },
+    // 计算负载
     calcLoadAvg(loadAvg, cores) {
       if (!loadAvg) {
         return 1;
@@ -90,6 +96,62 @@ export default {
         return 1
       }
       return loadAvg / cores * 100;
+    },
+    // 负载图表
+    drawLoadAvgChart() {
+      const data = [
+        { type: '1-3秒', value: 0.16 },
+        { type: '4-10秒', value: 0.125 },
+        { type: '11-30秒', value: 0.2 },
+        { type: '1-3分', value: 0.2 },
+        { type: '3-10分', value: 0.05 },
+        { type: '10-30分', value: 0.01 },
+        { type: '30+分', value: 0.015 },
+      ];
+
+      const chart = new Chart({
+        container: document.getElementById('container'),
+        autoFit: true,
+        height: 500,
+      });
+      chart.data(data);
+
+      chart.axis(false);
+      chart.tooltip({
+        showMarkers: false,
+      });
+      chart.coordinate('theta', { innerRadius: 0.35 });
+      chart.legend(false);
+      chart
+        .interval({
+          background: { style: { fill: '#F0F0F0', fillOpacity: 1 }},
+        })
+        .position('type*value')
+        .color('type', (val) => {
+          const { colors10 } = chart.getTheme();
+          if (val === '10-30分' || val === '30+分') {
+            return '#ff4d4f';
+          }
+          return colors10[0];
+        })
+        .style({
+          lineCap: 'round',
+          
+        });
+      chart.theme({
+        maxColumnWidth: 12,
+        minColumnWidth: 12,
+      })
+      chart.interaction('element-active')
+      chart.annotation().html({
+        position: ['50%', '50%'],
+        html: () => {
+          return `<div style="transform:translate(-50%,-50%)">
+            <img alt="" width="100" height="80" src="https://gw.alipayobjects.com/mdn/rms_ef85c6/afts/img/A*0DYiQKP08cQAAAAAAAAAAAAAARQnAQ">
+          </div>`
+        }
+      })
+      chart.render();
     }
   }
 }
